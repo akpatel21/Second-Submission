ce93899b80e2882424dbd5b699fdb5493898dc86
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2018-09-24 21:19:22 -0400

    Add files via upload

---
+++ SecondSubmission.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Akul Patel
+ * Second Submission
+ *
+ */
+public class SecondSubmission 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("My favorite subjects:\n\tComputer Science\n\tMath\n\tHistory");
+
+	}
+
+}
